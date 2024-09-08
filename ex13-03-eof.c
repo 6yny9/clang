3a5bfdfb412459cc5835e90f802e2324a52d716c
6yny9 <y_nayoung@naver.com>
2024-09-08 12:44:41 +0900

    2024-09-08

---
+++ ex13-03-eof.c
@@ -0,0 +1,28 @@
+/*
+EOF (End Of File)
+    EOF는 C프로그램에서 파일 끝을 나타내는 상수
+    EOF는 int형으로 정의되어 있으며, -1의 값이 할당되어 있다.
+
+EOF 입력방법:
+    입력 스트링(터미널)에서
+        Ctrl + Z (windows)
+        Ctrl + D (unix, Linux, macOS)
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int ch;
+
+    while(1)
+    {
+        ch = getchar();
+        if(ch == EOF) break;
+        putchar(ch);
+        putchar('|');
+    }
+
+    return 0;
+}
